e94d99d8ef577c027ad916852e062819b05bdd3b
mchp <piao.maria@gmail.com>
2020-03-26 22:46:12 -0700

    Add an example to test the checkstyle target

---
+++ example/Greeter.java
@@ -0,0 +1,8 @@
+package hello.world;
+
+public class Greeter {
+
+  public String greet() {
+    return "HelloWorld";
+  }
+}
---
+++ example/HelloWorld.java
@@ -0,0 +1,9 @@
+package hello.world;
+
+import hello.world.Greeter;
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println(new Greeter().greet());
+  }
+}
